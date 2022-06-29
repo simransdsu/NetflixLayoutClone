17c272e82e9931f569c5962cd743af1fe5a98b08
simransdsu <simran5590@gmail.com>
2022-06-28 20:23:39 -0700

    Initial Commit

---
+++ NetflixLayoutClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixLayoutClone
+//
+//  Created by Simran Preet Singh Narang on 2022-06-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
